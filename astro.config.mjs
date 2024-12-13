b08e4a984eb3697ce71ce9b9262f24873baa492c
Fei <taoshowad@gmail.com>
2024-12-14 07:36:14 +0800

    chore: remove the suffix of the generated file

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,4 @@ export default defineConfig({
 		// 忽略demos目录下页面的sitemap生成
 		filter: (page) => !page.includes('demos')
 	})],
-	trailingSlash: 'never',
-	build: {
-		// 示例：在构建过程中生 成`page.html` 而不是 `page/index.html`。
-		format: 'file'
-	}
 });
